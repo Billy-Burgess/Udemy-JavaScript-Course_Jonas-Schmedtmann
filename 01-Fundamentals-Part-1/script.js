133dd933c0ac4f283efdfe3280fd7efe56cc7e5f
B_Burgess <140908055+Billy-Burgess@users.noreply.github.com>
2023-08-27 17:50:22 +0100

    JS Course 2023 Udemy
    Slowly going through the course whilst completing the assignments

--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -1,5 +1,37 @@
-let js = 'amazing';
-if (js === 'amazing') alert('Javascript is FUN!');
+// let js = 'amazing';
+// if (js === 'amazing') alert('Javascript is FUN!');
 
 40 + 8 + 23 - 10;
 console.log(40 + 8 + 23 - 10);
+
+let firstName = 'Billy';
+console.log(firstName);
+
+
+// VALUES & VARIABLES
+
+let country = 'England';
+// let continent = 'Europe';
+let population = '55 million';
+console.log(country);
+// console.log(continent);
+console.log(population);
+
+// DATA TYPES
+
+let isIsland = 'England';
+let England = true;
+
+let language;
+
+console.log(isIsland);
+console.log(population);
+console.log(country);
+console.log(language);
+
+// let, const and var 
+
+language = 'english';
+// const isIsland = 'England'; //already declared above to 'let'
+const continent = 'Europe';
+console.log(continent);
