2d8fdc0255f68224dc0e12631628c6fba66aa87e
B_Burgess <140908055+Billy-Burgess@users.noreply.github.com>
2023-08-30 17:10:57 +0100

    Continuing with the course
    Completed part 1.

--- 01-Fundamentals-Part-1/Coding Challanges/script.js
+++ 01-Fundamentals-Part-1/Coding Challanges/script.js
@@ -17,3 +17,59 @@ const markHigherBMI = BMIMark > BMIJohn;
 // console.log(markHigherBMI);
 
 console.log(BMIMark, BMIJohn, markHigherBMI);
+
+// ***Coding Exercise 2: CHALLANGE #2
+
+let higherBMI;
+if (BMIMark > BMIJohn) {
+    higherBMI = `Mark's BMI (${BMIMark}) is higher than John's BMI (${BMIJohn})`;
+} else {
+    higherBMI = `John's BMI (${BMIJohn}) is higher than Mark's (${BMIMark})!`
+}
+console.log(higherBMI);
+
+// ***Coding Challenge #3 
+
+// test data Data 1: Dolphins score 96, 108 and 89. Koalas score 88, 91 and 110 
+const scoreDolphins = (96 + 108 + 89) / 3;
+const scoreKoalas = (88 + 91 + 110) / 3;
+console.log(scoreDolphins);
+console.log(scoreKoalas);
+
+if (scoreDolphins > scoreKoalas && scoreDolphins >= 100) {
+    console.log("Dolphins win the trophy");
+} else if (scoreDolphins < scoreKoalas && scoreKoalas >= 100) {
+    console.log("Koalas win the trophy");
+} else if (scoreKoalas === scoreDolphins && scoreDolphins >= 100 && scoreKoalas >= 100) {
+    console.log("Both win the trophy");
+} else {
+    console.log("No winners!")
+}
+
+// test data Data Bonus 2: Dolphins score 97, 112 and 101. Koalas score 109, 95 and 106 
+const scoreDolphins2 = (97 + 112 + 101) / 3;
+const scoreKoalas2 = (109 + 95 + 106) / 3;
+console.log(scoreDolphins2);
+console.log(scoreKoalas2);
+
+if (scoreDolphins2 > scoreKoalas2 && scoreDolphins2 >= 100) {
+    console.log("Dolphins win the trophy");
+} else if (scoreDolphins2 < scoreKoalas2 && scoreKoalas2 >= 100) {
+    console.log("Koalas win the trophy");
+} else if (scoreKoalas2 === scoreDolphins2 && scoreDolphins2 >= 100 && scoreKoalas2 >= 100) {
+    console.log("Both win the trophy");
+} else {
+    console.log("No winners!")
+}
+
+// Coding Challenge #4
+const bill = 275;
+const tip = bill >= 50 && bill < 300 ? bill * 0.15 : bill * 0.2;
+console.log(tip);
+console.log(`The bill was ${bill}, the tip was ${tip}, so the total value is ${bill + tip}`);
+
+
+
+
+
+
--- 01-Fundamentals-Part-1/Walkthrough Assignments/script.js
+++ 01-Fundamentals-Part-1/Walkthrough Assignments/script.js
@@ -19,10 +19,10 @@ console.log(population);
 
 // ***DATA TYPES
 
-let isIsland = 'England';
+let isIsland = true;
 let England = true;
 
-let language;
+let language = "english";
 
 console.log(isIsland);
 console.log(population);
@@ -31,7 +31,7 @@ console.log(language);
 
 // ***let, const and var 
 
-language = 'english';
+// language = 'english';
 // const isIsland = 'England'; //already declared above to 'let'
 const continent = 'Europe';
 console.log(continent);
@@ -43,5 +43,118 @@ console.log(population + 1);
 console.log(population > 600000);
 console.log(population < 33000000);
 
-const description = 'England is in Europe, and its 55 million people speak english';
+// const description = 'England is in Europe, and its 55 million people speak english';
+
+// ***Strings and Template Literals 
+const description = `${country} is in ${continent} and it's ${population} million people speak ${language}.`
+
+// ***If / Else statements
+
+const age = 15;
+
+if (age >= 18) {
+    console.log('Sarah can start driving licence')
+} else {
+    const yearsLeft = 18 - age;
+    console.log(`Sarah is too young. Wait another ${yearsLeft} years`);
+}
+
+const birthYear = 2005;
+
+let century;
+if (birthYear <= 2000) {
+    century = 20;
+} else {
+    century = 21;
+    console.log(century);
+}
+
+// ***Type Conversion and Coercion
+
+// Predict the result of these 5 operations without executing them: 
+// '9' - '5'; 
+// '19' - '13' + '17'; 
+// '19' - '13' + 17; 
+// '123' < 57; 
+// 5 + 6 + '4' + 9 - 4 - 2;
+
+console.log('9' - '5');
+console.log('19' - '13' + '17');
+console.log('19' - '13' + 17);
+console.log('123' < 57);
+console.log(5 + 6 + '4' + 9 - 4 - 2);
+
+// 4
+// 617
+// 23
+// false
+// 1143
+
+// Examples below:
+// console.log('23' / '2');
+// console.log('23' * '2');
+// console.log('23' + '2');
+// console.log(23 + 2);
+// console.log('23' + 2);
+// console.log(23 - 2);
+// console.log('23' - '2');
+
+// ***Equality Operators: == vs. === 
+
+// const numNeighbours = Number(prompt("How many neighbour countries does your country have?"));
+// if (numNeighbours === 1) {
+//     console.log('Only 1 border!')
+// } else if (numNeighbours > 1) {
+//     console.log('More than 1 border.')
+// } else if (numNeighbours === 0)
+//     console.log('No borders.')
+
+const hasDriversLicence = false;
+const hasGoodVision = false;
+console.log(hasDriversLicence && hasGoodVision);
+console.log(hasDriversLicence || hasGoodVision);
+console.log(hasDriversLicence || hasGoodVision);
+console.log(!hasDriversLicence);
+
+// ***Logical Operators 
+// Sarah is looking for a new country to live in. She wants to live in a country that speaks english, 
+// has less than 50 million people and is not an island. 
+
+if (language === "english" && population < 50000000 && isIsland === false) {
+    console.log("You should live in England.")
+} else {
+    console.log("England does not match your criteria, sorry.")
+}
+
+// ***The switch statement
+
+const popularLanguage = "chinese";
+
+switch (popularLanguage) {
+    case "chinese":
+    case "mandarin":
+        console.log("MOST number of native speakers!");
+        break;
+    case "spanish":
+        console.log("2nd place in number of native speakers");
+        break;
+    case "english":
+        console.log("3rd place");
+        break;
+    case "hindi":
+        console.log("Number 4");
+        break;
+    case "arabic":
+        console.log("5th most spoken");
+        break;
+    default:
+        console.log("great language too");
+}
+
+// ***The Conditional (Ternary) Operator
+
+console.log(`${country}'s population is ${population > 33000000 ? "above" : "below"} average`);
+
+const drink = age >= 18 ? "wine" : "water"
+console.log(drink);
 
