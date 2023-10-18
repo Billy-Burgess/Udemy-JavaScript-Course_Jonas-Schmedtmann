8be2281e150e3c16500b8b228d1a0d4b8e4e3e7d
B_Burgess <140908055+Billy-Burgess@users.noreply.github.com>
2023-10-18 16:54:50 +0100

    Starting Fundamentals Part 2

---
+++ 01-Fundamentals-Part-2/Walkthrough Assignments/script.js
@@ -0,0 +1,8 @@
+'use strict';
+
+let hasDriversLicense = false;
+const passTest = true;
+
+if (passTest) hasDriverLicense = true; //missing the s
+if (hasDriversLicense) console.log('I can drive');
+
